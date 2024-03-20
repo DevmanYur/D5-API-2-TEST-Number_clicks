27f6889548a490e6727b6e84c5e67e199291e9ed
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-03-20 19:13:41 +0300

    ya.ru

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-import requests
-import json
+# import requests
+#
+# url = 'https://api-ssl.bitly.com/v4/user'
+# headers = {
+#     'Authorization': 'Bearer d8f3784dc29d4dbab3752c20b2076bf878e9e524',
+# }
+#
+# response = requests.get(url, headers=headers)
+#
+# print(response.json())
 
-url = 'https://api-ssl.bitly.com/v4/user'
+
+import requests
+
 headers = {
-     'Authorization': 'Bearer d8f3784dc29d4dbab3752c20b2076bf878e9e524',
- }
-response = requests.get(url, headers=headers)
+    'Authorization': 'Bearer d8f3784dc29d4dbab3752c20b2076bf878e9e524',
+    'Content-Type': 'application/json',
+}
+
+data = '{ "long_url": "https://ya.ru/" }'
+
+response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
+
 print(response.json())
+
+
+
+
 
