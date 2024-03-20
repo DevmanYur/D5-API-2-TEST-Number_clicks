57348c6217a9874dc1a3fd305af9a9efa40d14df
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-03-20 18:21:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import requests
+import json
 
 url = 'https://api-ssl.bitly.com/v4/user'
 headers = {
-    'Authorization': 'Bearer d8f3784dc29d4dbab3752c20b2076bf878e9e524',
-}
-
+     'Authorization': 'Bearer d8f3784dc29d4dbab3752c20b2076bf878e9e524',
+ }
 response = requests.get(url, headers=headers)
-
 print(response.json())
 
