ca36635a3984c1ea9201395a9386308541b5c47e
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-03-20 17:38:44 +0300

    up

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://api-ssl.bitly.com/v4/user'
+headers = {
+    'Authorization': 'Bearer d8f3784dc29d4dbab3752c20b2076bf878e9e524',
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.json())
+
